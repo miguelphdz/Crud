ac770f07a802302da2317b2b4f7bc4eece7c7f63
miguelphdz <137126442+miguelphdz@users.noreply.github.com>
2024-09-20 23:40:02 -0600

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.listen(app.get('port'), () => {
+    console.log('Server on port 300');
+}); 
